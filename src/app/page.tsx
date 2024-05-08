1ca2b997e24de5045bc5ebb1263d3c2dfec53684
hussainlinkstar <106742311+hussainlinkstar@users.noreply.github.com>
2024-05-08 17:46:56 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-         dev
+         dev edits
       </div>
     </main>
   );
